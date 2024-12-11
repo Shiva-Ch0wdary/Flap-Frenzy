32283def8b2b211dd1ddbbc4409daf2874f3d26e
Spandana-25 <139213427+Spandana-25@users.noreply.github.com>
2024-12-11 23:07:38 +0530

    unlock characters bugs

--- Assets/Scripts/CharacterSelect.cs
+++ Assets/Scripts/CharacterSelect.cs
@@ -7,6 +7,7 @@ public class CharacterSelect : MonoBehaviour
     public Button[] lockButtons; // Lock buttons for each character
     public Button selectButton; // Button to select the character
     public int selectedCharacter;
+    public Text selectButtonText; // Text component of the Select button
 
     public int[] unlockCosts; // Coins required to unlock each character
     public GameObject CharacterPrefab;
@@ -58,118 +59,159 @@ public class CharacterSelect : MonoBehaviour
     }
 
     public void ResetCharacterUI()
-{
-    // Reset character states
-    selectedCharacter = 0; // Default to the first character
-    PlayerPrefs.SetInt("SelectedCharacter", 0); // Save the default selection
-
-    for (int i = 0; i < skins.Length; i++)
     {
-        skins[i].SetActive(false);
-        lockButtons[i].gameObject.SetActive(i != 0); // Show lock buttons for all except the first character
-    }
+        // Reset character states
+        selectedCharacter = 0; // Default to the first character
+        PlayerPrefs.SetInt("SelectedCharacter", 0); // Save the default selection
 
-    skins[selectedCharacter].SetActive(true); // Activate the first character skin
-    UpdateUI(); // Refresh UI
-}
+        for (int i = 0; i < skins.Length; i++)
+        {
+            skins[i].SetActive(false);
+            lockButtons[i].gameObject.SetActive(i != 0); // Show lock buttons for all except the first character
+        }
+
+        skins[selectedCharacter].SetActive(true); // Activate the first character skin
+        UpdateUI(); // Refresh UI
+    }
 
 
     private void UpdateUI()
-{
-    // Update lock buttons and coins needed text
-    for (int i = 0; i < lockButtons.Length; i++)
     {
-        lockButtons[i].gameObject.SetActive(i == selectedCharacter && !IsUnlocked(i));
-    }
+        for (int i = 0; i < lockButtons.Length; i++)
+        {
+            bool isCharacterUnlocked = IsUnlocked(i);
+            bool isSelectedCharacter = (i == selectedCharacter);
 
-    // Update select button interactability
-    if (IsUnlocked(selectedCharacter))
-    {
-        selectButton.interactable = true;
-        coinsNeededText.gameObject.SetActive(false); // Hide the coins needed text when unlocked
+            // Update lock button visibility
+            lockButtons[i].gameObject.SetActive(!isCharacterUnlocked && isSelectedCharacter);
+
+            // Update the button text and interactivity
+            if (isSelectedCharacter)
+            {
+                if (isCharacterUnlocked)
+                {
+                    // If this character is currently selected, update button text to "Selected"
+                    if (PlayerPrefs.GetInt("SelectedCharacter", 0) == i)
+                    {
+                        selectButton.interactable = false; // Disable the button
+                        selectButtonText.text = "Selected";
+                    }
+                    else
+                    {
+                        selectButton.interactable = true; // Enable the button
+                        selectButtonText.text = "Select";
+                    }
+                }
+                else
+                {
+                    // Display the coins needed to unlock on the button
+                    int totalCoins = PlayerPrefs.GetInt("TotalCoins", 0);
+                    int cost = unlockCosts[i];
+
+                    selectButtonText.text = $"Unlock ({cost} Coins)";
+                    selectButton.interactable = totalCoins >= cost; // Enable only if enough coins
+                }
+            }
+        }
+
+        // Hide the coinsNeededText (if used separately) since it’s no longer needed
+        coinsNeededText.gameObject.SetActive(false);
+
+
+
+        // Update coinsNeededText visibility and content
+        if (IsUnlocked(selectedCharacter))
+        {
+            coinsNeededText.gameObject.SetActive(false); // Hide text if unlocked
+        }
+        else
+        {
+            coinsNeededText.gameObject.SetActive(true); // Show required coins if locked
+            coinsNeededText.text = $"Coins Needed: {unlockCosts[selectedCharacter]}";
+        }
     }
-    else
-    {
-        selectButton.interactable = false;
-        int cost = unlockCosts[selectedCharacter];
-        coinsNeededText.gameObject.SetActive(true); // Show the coins needed text when locked
-        coinsNeededText.text = "Coins Needed: " + cost.ToString(); // Display the required coins
-    }
-}
 
 
 
     public void OnSelectCharacter()
 {
-    int totalCoins = PlayerPrefs.GetInt("TotalCoins", 0);
-    int cost = unlockCosts[selectedCharacter];
 
-    if (IsUnlocked(selectedCharacter))
-    {
-        PlayerPrefs.SetInt("SelectedCharacter", selectedCharacter);
-        Debug.Log(skins[selectedCharacter].name + " Selected!");
-    }
-    else if (totalCoins >= cost)
-    {
-        // Deduct coins and unlock character
-        totalCoins -= cost;
-        PlayerPrefs.SetInt("TotalCoins", totalCoins);
-        PlayerPrefs.SetInt("CharacterUnlocked" + selectedCharacter, 1);
-        PlayerPrefs.SetInt("SelectedCharacter", selectedCharacter);
-        Debug.Log(skins[selectedCharacter].name + " Unlocked and Selected!");
+        int totalCoins = PlayerPrefs.GetInt("TotalCoins", 0);
+        int cost = unlockCosts[selectedCharacter];
 
-        // Update coin display if needed
-        StartMenuCoins coinManager = FindObjectOfType<StartMenuCoins>();
-        if (coinManager != null)
+        if (IsUnlocked(selectedCharacter))
         {
-            coinManager.UpdateCoinDisplay();
-        }
+            // Save the selected character
+            PlayerPrefs.SetInt("SelectedCharacter", selectedCharacter);
+            Debug.Log(skins[selectedCharacter].name + " Selected!");
 
-        // Refresh UI
-        UpdateUI();
+            // Update the UI to reflect the selection
+            UpdateUI();
+        }
+        else if (totalCoins >= cost)
+        {
+            // Deduct coins, unlock, and select the character
+            totalCoins -= cost;
+            PlayerPrefs.SetInt("TotalCoins", totalCoins);
+            PlayerPrefs.SetInt("CharacterUnlocked" + selectedCharacter, 1); // Unlock character
+            PlayerPrefs.SetInt("SelectedCharacter", selectedCharacter); // Select character
+
+            Debug.Log(skins[selectedCharacter].name + " Unlocked and Selected!");
+
+            // Update UI and other references
+            UpdateUI();
+
+            // Optionally update the coin display
+            StartMenuCoins coinManager = FindObjectOfType<StartMenuCoins>();
+            if (coinManager != null)
+            {
+                coinManager.UpdateCoinDisplay();
+            }
+        }
+        else
+        {
+            Debug.Log("Not enough coins to unlock " + skins[selectedCharacter].name);
+        }
+        gameObject.SetActive(true);
     }
-    else
-    {
-        Debug.Log("Not have enough coins to unlock ");
-    }
-}
 
 
     public void OnUnlockCharacter()
 {
-    int totalCoins = PlayerPrefs.GetInt("TotalCoins", 0); // Get current coin count
-    int cost = unlockCosts[selectedCharacter]; // Get unlock cost for the selected character
+    //int totalCoins = PlayerPrefs.GetInt("TotalCoins", 0); // Get current coin count
+    //int cost = unlockCosts[selectedCharacter]; // Get unlock cost for the selected character
 
-    // Check if there are enough coins
-    if (totalCoins >= cost && !IsUnlocked(selectedCharacter))
-    {
-        totalCoins -= cost; // Deduct coins
-        PlayerPrefs.SetInt("TotalCoins", totalCoins); // Save updated coin count
-        PlayerPrefs.SetInt("CharacterUnlocked" + selectedCharacter, 1); // Mark character as unlocked
+    //// Check if there are enough coins
+    //if (totalCoins >= cost && !IsUnlocked(selectedCharacter))
+    //{
+    //    totalCoins -= cost; // Deduct coins
+    //    PlayerPrefs.SetInt("TotalCoins", totalCoins); // Save updated coin count
+    //    PlayerPrefs.SetInt("CharacterUnlocked" + selectedCharacter, 1); // Mark character as unlocked
 
-        Debug.Log(skins[selectedCharacter].name + " unlocked!");
+    //    Debug.Log(skins[selectedCharacter].name + " unlocked!");
 
-        // Update UI: Hide lock button and enable the select button
-        lockButtons[selectedCharacter].gameObject.SetActive(false); // Hide the lock button
-        selectButton.interactable = true; // Enable the select button
+    //    // Update UI: Hide lock button and enable the select button
+    //    lockButtons[selectedCharacter].gameObject.SetActive(false); // Hide the lock button
+    //    selectButton.interactable = true; // Enable the select button
 
-        // Update the coinsNeededText to show "Character Unlocked!" message
-        coinsNeededText.text = " Character Unlocked!";
-        coinsNeededText.gameObject.SetActive(true); // Make sure it's visible
+    //    // Update the coinsNeededText to show "Character Unlocked!" message
+    //    coinsNeededText.text = " Character Unlocked!";
+    //    coinsNeededText.gameObject.SetActive(true); // Make sure it's visible
 
-        // Optionally, update coin display (if in the same scene as StartMenuCoins)
-        StartMenuCoins coinManager = FindObjectOfType<StartMenuCoins>();
-        if (coinManager != null)
-        {
-            coinManager.UpdateCoinDisplay();
-        }
-    }
-    else
-    {
-        Debug.Log("Not enough coins to unlock " + skins[selectedCharacter].name);
-        coinsNeededText.text = "Not enough coins to unlock " + skins[selectedCharacter].name;
-        coinsNeededText.gameObject.SetActive(true); // Show the "Not enough coins" message
-    }
+    //    // Optionally, update coin display (if in the same scene as StartMenuCoins)
+    //    StartMenuCoins coinManager = FindObjectOfType<StartMenuCoins>();
+    //    if (coinManager != null)
+    //    {
+    //        coinManager.UpdateCoinDisplay();
+    //    }
+   // }
+   // }
+    //else
+    //{
+    //    Debug.Log("Not enough coins to unlock " + skins[selectedCharacter].name);
+    //    coinsNeededText.text = "Not enough coins to unlock " + skins[selectedCharacter].name;
+    //    coinsNeededText.gameObject.SetActive(true); // Show the "Not enough coins" message
+    //}
 }
 
 
